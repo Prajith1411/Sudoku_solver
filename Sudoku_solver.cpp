72d4f8ae0561203e95db557c2bbb27ba22e18aca
Prajith1411 <reddysivaprajith1411@gmail.com>
2025-06-28 10:36:09 +0530

    Update Sudoku_solver.cpp

--- Sudoku_solver.cpp
+++ Sudoku_solver.cpp
@@ -1,58 +1,68 @@
 #include <iostream>
-
 #include <vector>
-
+#include <fstream>
 using namespace std;
 
-bool isValid(vector < vector < char >> & board, int row, int col, char c) {
-  for (int i = 0; i < 9; i++) {
-    if (board[i][col] == c)
-      return false;
-
-    if (board[row][i] == c)
-      return false;
-
-    if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c)
-      return false;
-  }
-  return true;
+bool isValid(vector<vector<char>> &board, int row, int col, char c) {
+    for (int i = 0; i < 9; i++) {
+        if (board[i][col] == c) return false;
+        if (board[row][i] == c) return false;
+        if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c) return false;
+    }
+    return true;
 }
 
-bool solveSudoku(vector < vector < char >> & board) {
-  for (int i = 0; i < board.size(); i++) {
-    for (int j = 0; j < board[0].size(); j++) {
-      if (board[i][j] == '.') {
-        for (char c = '1'; c <= '9'; c++) {
-          if (isValid(board, i, j, c)) {
-            board[i][j] = c;
-
-            if (solveSudoku(board))
-              return true;
-            else
-              board[i][j] = '.';
-          }
+bool solveSudoku(vector<vector<char>> &board) {
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            if (board[i][j] == '.') {
+                for (char c = '1'; c <= '9'; c++) {
+                    if (isValid(board, i, j, c)) {
+                        board[i][j] = c;
+                        if (solveSudoku(board)) return true;
+                        board[i][j] = '.';
+                    }
+                }
+                return false;
+            }
         }
-
-        return false;
-      }
     }
-  }
-  return true;
+    return true;
 }
+
 int main() {
-    vector<vector<char>>board(9,vector<char>(9));
-    for(int i=0;i<9;i++){
-      for(int j=0;j<9;j++){
-        cin>>board[i][j];
-      }
+    ifstream input("input.txt");
+    ofstream output("output.txt");
+
+    if (!input.is_open()) {
+        cerr << "Could not open input.txt" << endl;
+        return 1;
     }
-   
-    solveSudoku(board);
-        	
-    for(int i= 0; i< 9; i++){
-        for(int j= 0; j< 9; j++)
-            cout<<board[i][j]<<" ";
-            cout<<endl;
+    if (!output.is_open()) {
+        cerr << "Could not open output.txt" << endl;
+        return 1;
     }
+
+    vector<vector<char>> board(9, vector<char>(9));
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            string val;
+            input >> val;
+            board[i][j] = (val == ".") ? '.' : val[0];
+        }
+    }
+
+    if (solveSudoku(board)) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++)
+                output << board[i][j] << " ";
+            output << endl;
+        }
+    } else {
+        output << "No solution exists" << endl;
+    }
+
+    input.close();
+    output.close();
     return 0;
 }
